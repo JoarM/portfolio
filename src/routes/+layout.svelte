fc6007344559656732002d822efec82a560569be
Joar Maltesson <joar05@hotmail.com>
2024-03-12 19:12:46 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+    let atTop = false;
+</script>
+
+<header class:not-top={atTop}>
+    <div>
+        <a href="/">Joar Maltesson</a>
+        <nav>
+            <a href="/projects">Projects</a>
+            <a href="about-me">About me</a>
+            <a href="contact-me">Contact me</a>
+        </nav>
+    </div>
+</header>
+
+<slot/>
+<svelte:window on:scroll={(e) => atTop = !!e.currentTarget.scrollY} />
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Truculenta:opsz,wght@12..72,100..900&display=swap');
+
+    :global(*) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :global(:root) {
+        --background: 210 5.6% 9%;
+        --foreground: 210 0% 93%;
+        --border: 210 7.4% 34%;
+        --muted-foreground: 210 7.4% 54%;
+    }
+
+    :global(html) {
+        background-color: hsl(var(--background));
+        color: hsl(var(--foreground));
+        font-family: "Truculenta", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+        font-variation-settings:
+            "wdth" 100;
+    }
+
+    header {
+        position: sticky;
+        top: 0;
+        width: 100%;
+        padding-block: 1.25rem;
+        backdrop-filter: blur(3px);
+        border-bottom: 1px solid hsl(var(--border)/0);
+        transition: border-bottom 250ms;
+        z-index: 50;
+    }
+
+    header > div {
+        max-width: 1240px;
+        width: 100%;
+        margin-inline: auto;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding-inline: 1rem;
+    }
+
+    nav {
+        display: flex;
+        align-items: center;
+        gap: .75rem;
+    }
+
+    a {
+        color: inherit;
+        text-underline-offset: 4px;
+    }
+
+    div > a {
+        font-size: 1.5rem;
+        font-weight: bold;
+        text-decoration: none;
+    }
+
+    nav > a {
+        font-size: 500;
+    }
+
+    .not-top {
+        border-bottom: 1px solid hsl(var(--border));
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,77 @@
+<section class="hero">
+    <div class="hero-info">
+        <h1>Joar Maltesson</h1>
+        <p>I develop software that scales.</p>
+    </div>
+</section>
+
+<section class="contact">
+    <div class="contact-wrapper">
+        <h2>Contact Me</h2>
+    </div>
+</section>
+
+<style>
+    @keyframes grow-in {
+        from { scale: 0.9; }
+        to { scale: 1; }
+    }
+
+    .hero {
+        display: grid;
+        max-width: 1240px;
+        padding-inline: 1rem;
+        margin-inline: auto;
+        min-height: calc(100vh - 74.4px);
+        min-height: calc(100dvh - 74.4px);
+    }
+
+    .hero-info {
+        align-self: center;
+    }
+
+    .hero-info > h1 {
+        font-size: 3.75rem;
+        font-size: bold;
+    }
+
+    .hero-info > p {
+        font-size: 1.625rem;
+        font-size: 500;
+        color: hsl(var(--muted-foreground));
+    }
+
+    .contact {
+        min-height: 100vh;
+        min-height: 100dvh;
+        position: relative;
+    }
+
+    .contact::before {
+        content: "";
+        position: absolute;
+        inset: 0;
+        background-image: url("/TheLog.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
+        filter: grayscale(100);
+        opacity: 65%;
+        z-index: -1;
+        animation: grow-in linear;
+        animation-timeline: view();
+        animation-range-start: normal;
+        animation-range-end: contain;
+    }
+
+    .contact-wrapper {
+        max-width: 1240px;
+        margin-inline: auto;
+        padding-block: 8rem;
+        padding-inline: 1rem;
+    }
+
+    .contact-wrapper > h2 {
+        font-size: 2.75rem;
+        font-weight: bold;
+    }
+</style>
